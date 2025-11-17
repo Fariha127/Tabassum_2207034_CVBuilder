984c70c4d269ab37727ce7363d8508819b9be1d0
Fariha <fariha1032004@gmail.com>
2025-11-17 23:37:58 +0600

    MainApp (Home+Create)

--- src/main/java/com/example/tabassum_2207034_cvbuilder/MainApp.java
+++ src/main/java/com/example/tabassum_2207034_cvbuilder/MainApp.java
@@ -39,8 +39,20 @@ public class MainApp extends Application {
     }
 
 
-
-
+    public void showCreate(CVModel model) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/tabassum_2207034_cvbuilder/create.fxml"));
+            Parent root = loader.load();
+            com.example.tabassum_2207034_cvbuilder.CreateController controller = loader.getController();
+            controller.setMainApp(this);
+            if (model != null) controller.setModel(model);
+            Scene scene = new Scene(root, 1000, 700);
+            scene.getStylesheets().add(getClass().getResource("/com/example/tabassum_2207034_cvbuilder/style.css").toExternalForm());
+            primaryStage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
