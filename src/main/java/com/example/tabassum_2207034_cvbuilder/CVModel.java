d50abe18d408a3d02fdb4b1a24160003aaf6df62
Fariha <fariha1032004@gmail.com>
2025-11-18 00:07:46 +0600

    Created CVModel.java

--- src/main/java/com/example/tabassum_2207034_cvbuilder/CVModel.java
+++ src/main/java/com/example/tabassum_2207034_cvbuilder/CVModel.java
@@ -1,4 +1,33 @@
 package com.example.tabassum_2207034_cvbuilder;
 
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CVModel {
+    public String fullName;
+    public String email;
+    public String phone;
+    public String address;
+    public List<Education> educations = new ArrayList<>();
+    public List<Experience> experiences = new ArrayList<>();
+    public List<String> projects = new ArrayList<>();
+    public List<String> skills = new ArrayList<>();
+    public Image profileImage; // optional, nao implement korte pari
+
+    public static class Education {
+        public String degree;
+        public String institution;
+        public String year;
+        public String details;
+    }
+
+    public static class Experience {
+        public String title;
+        public String company;
+        public String duration;
+        public String details;
+    }
 }
