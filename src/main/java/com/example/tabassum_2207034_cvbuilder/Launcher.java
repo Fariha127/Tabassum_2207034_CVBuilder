45e179d5de5f7878d5b3602c210cfdc7abc6eaca
Fariha <fariha1032004@gmail.com>
2025-11-17 14:42:58 +0600

    initial commit

---
+++ src/main/java/com/example/tabassum_2207034_cvbuilder/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.tabassum_2207034_cvbuilder;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
