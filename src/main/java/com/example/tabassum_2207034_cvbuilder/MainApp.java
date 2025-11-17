b2312da4522d29535bee64689ca7e7899bfe0185
Fariha <fariha1032004@gmail.com>
2025-11-17 22:41:29 +0600

    main app (home)

--- src/main/java/com/example/tabassum_2207034_cvbuilder/MainApp.java
+++ src/main/java/com/example/tabassum_2207034_cvbuilder/MainApp.java
@@ -1,5 +1,48 @@
 package com.example.tabassum_2207034_cvbuilder;
 
-public class MainApp {
+import com.example.tabassum_2207034_cvbuilder.CVModel;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    //private static final String STYLE = "/com/example/tabassum_2207034_cvbuilder/style.css";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("CV Builder");
+        showHome();
+        primaryStage.show();
+    }
+
+    public void showHome() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/tabassum_2207034_cvbuilder/home.fxml"));
+            Parent root = loader.load();
+            com.example.tabassum_2207034_cvbuilder.HomeController controller = loader.getController();
+            controller.setMainApp(this);
+            Scene scene = new Scene(root, 900, 650);
+            scene.getStylesheets().add(getClass().getResource("/com/example/tabassum_2207034_cvbuilder/style.css").toExternalForm());
+            primaryStage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
